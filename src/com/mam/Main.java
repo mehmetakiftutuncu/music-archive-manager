3e3d59288392aa975186bbeb684b22967339b8bb
m.akif.tutuncu@gmail.com <m.akif.tutuncu@gmail.com@30ada0b5-b959-7eca-9633-e4131a9d76ad>
2013-04-07 18:01:11 +0000

    Initial commit

---
+++ src/com/mam/Main.java
@@ -0,0 +1,14 @@
+package com.mam;
+
+/**
+ * @author Ezgi Hacýhalil
+ * @author Mehmet Akif Tütüncü
+ * @author Rhahadian Bima Saputra
+ */
+public class Main
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello world!");
+	}
+}
